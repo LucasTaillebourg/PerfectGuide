a7bea3bad293fe203a5a64163847567073b410f4
Lucas TAILLEBOURG <lucas.taillebourg@groupama.com>
2019-03-11 15:18:47 +0100

    Initial commit

---
+++ back-end/src/main/kotlin/com/anima/AnimaPerfectGuide/AnimaPerfectGuideApplication.kt
@@ -0,0 +1,11 @@
+package com.anima.AnimaPerfectGuide
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnimaPerfectGuideApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnimaPerfectGuideApplication>(*args)
+}
---
+++ back-end/src/test/kotlin/com/anima/AnimaPerfectGuide/AnimaPerfectGuideApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.anima.AnimaPerfectGuide
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class AnimaPerfectGuideApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
